4bcf3832567867b5213758f66c0bc12581c5fef1
Emanuele <Casi>
2020-10-02 16:03:31 +0200

    upload menu and burger

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Form from './src/components/form/form.js';
-
+import { theme } from './src/themes.js';
+import { GlobalStyles } from './src/global.js';
+import { ThemeProvider } from 'styled-components';
+import { Burger } from './src/components/burger/burger.js';
+import { Menu } from './src/components/menu/menu.js';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Form></Form>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+      return (
+          <ThemeProvider theme={theme}>
+            <GlobalStyles />
+
+             <View >
+                <Form></Form>
+                <StatusBar style="auto" />
+              </View>
+            
+          </ThemeProvider>
+      );
 }
 
 const styles = StyleSheet.create({
